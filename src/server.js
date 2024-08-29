f9249787cfff3e73dfba3efb8c8497f43f7986b5
Evelyn Oliveira <evelynoliveira.tds1.senai@gmail.com>
2024-08-29 15:51:45 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -9,8 +9,55 @@ const app = express()
 
 app.use(express.json())
 
+const guloseimas = [
+    {
+        id: 1,
+        nome: "Trufa",
+        preco: 8.50,
+    },
+    {
+        id: 2,
+        nome: "Brigadeiro",
+        preco: 4.5,
+    },
+    {
+        id:3,
+        nome: "Fini",
+        preco: 12.90,
+    }
+]
+
+const filmesMarcantes = [
+    {
+        id: 1001,
+        titulo: "Bad Boys",
+        genero: "Ação",
+        emCartaz: false,
+    },
+    {
+        id: 1002,
+        titulo: "Deadpool & Wolverine",
+        genero: "Ação",
+        emCartaz: true,
+    },
+    {
+        id: 1003,
+        titulo: "É assim que acaba",
+        genero: "Romance",
+        emCartaz: true,
+    }
+]
+
 app.get("/", (req, res) => {
     return res.status(200).send({message: "Hello World!"})
+})
+
+app.get("/doces", (req, res) => {
+    return res.status(200).send(guloseimas)
+})
+
+app.get("/filmes", (req, res) => {
+    return res.status(200).send(filmesMarcantes)
 })
 
 app.listen(port, () => {
