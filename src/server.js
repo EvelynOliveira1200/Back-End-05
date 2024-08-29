16fcddc9cad1796ff83e9bbb4d890fa077c1f4a7
Evelyn Oliveira <evelynoliveira.tds1.senai@gmail.com>
2024-08-29 17:09:11 -0300

    refactor: alter doces routes structure

--- src/server.js
+++ src/server.js
@@ -9,23 +9,6 @@ const app = express()
 
 app.use(express.json())
 
-const guloseimas = [
-    {
-        id: 1,
-        nome: "Trufa",
-        preco: 8.50,
-    },
-    {
-        id: 2,
-        nome: "Brigadeiro",
-        preco: 4.5,
-    },
-    {
-        id:3,
-        nome: "Fini",
-        preco: 12.90,
-    }
-]
 
 const filmesMarcantes = [
     {
@@ -52,22 +35,6 @@ app.get("/", (req, res) => {
     return res.status(200).send({message: "Hello World!"})
 })
 
-app.get("/doces", (req, res) => {
-    return res.status(200).send(guloseimas)
-})
-
-app.post("/doces", (req, res) => {
-    const{nome, preco} = req.body; 
-
-    const novoDoce = {
-        id: guloseimas.length + 1, 
-        nome: nome,
-        preco: preco,
-    };
-
-    guloseimas.push(novoDoce)
-    return res.status(201).send(guloseimas)
-})
 
 app.get("/filmes", (req, res) => {
     return res.status(200).send(filmesMarcantes)
