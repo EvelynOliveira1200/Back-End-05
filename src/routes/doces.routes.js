8c048f9d1cda21b4634e3678dca42d54f4bfef15
Evelyn Oliveira <evelynoliveira.tds1.senai@gmail.com>
2024-09-05 09:52:33 -0300

    feat: get by id method

--- src/routes/doces.routes.js
+++ src/routes/doces.routes.js
@@ -14,21 +14,23 @@ const guloseimas = [
         preco: 4.5,
     },
     {
-        id:3,
+        id: 3,
         nome: "Fini",
         preco: 12.90,
     }
 ]
 
+// Rota para buscar todos os elementos do array guloseimas
 docesRoutes.get("/", (req, res) => {
     return res.status(200).send(guloseimas)
 })
 
+// Rota para criar nova guloseima
 docesRoutes.post("/", (req, res) => {
-    const{nome, preco} = req.body; 
+    const { nome, preco } = req.body;
 
     const novoDoce = {
-        id: guloseimas.length + 1, 
+        id: guloseimas.length + 1,
         nome: nome,
         preco: preco,
     }
@@ -36,6 +38,25 @@ docesRoutes.post("/", (req, res) => {
     guloseimas.push(novoDoce)
     return res.status(201).send(guloseimas)
 
+})
+
+// Rota para buscar um elemento específico do array guloseimas
+docesRoutes.get("/:id", (req, res) => {
+    const { id } = req.params
+
+    console.log(id);
+    
+
+    const guloseima = guloseimas.find((doce) => doce.id === Number (id)
+    )
+
+    console.log(guloseima)
+
+    if (!guloseima){
+        return res.status(404).send({message: "Guloseima não encontrada!"})
+    }
+
+    return res.status(200).send(guloseima)
 })
 
 export default docesRoutes
