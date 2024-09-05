3960077b5a043e2acc0ff0fa2f179695dc488e4f
Evelyn Oliveira <evelynoliveira.tds1.senai@gmail.com>
2024-09-05 11:15:35 -0300

    feat: create put method

--- src/server.js
+++ src/server.js
@@ -8,9 +8,10 @@ const port = process.env.PORT || 3000
 
 const app = express()
 
+app.use(express.json())
+
 app.use(routes)
 
-app.use(express.json())
 
 app.listen(port, () => {
     console.log(`🦋 Server started on http://localhost:${port}`)
